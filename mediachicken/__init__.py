adf1937fa8182875fbf443839d338890795d2797
Garet McKinley <garetmckinley@me.com>
2014-09-30 21:38:54 -0400

    Added main app to __init__

---
+++ mediachicken/__init__.py
@@ -0,0 +1,25 @@
+from flask import Flask
+from flask.ext.cache import Cache
+from werkzeug.routing import BaseConverter
+import redis
+import os
+
+
+class RegexConverter(BaseConverter):
+
+    def __init__(self, url_map, *items):
+        super(RegexConverter, self).__init__(url_map)
+        self.regex = items[0]
+
+
+app = Flask(__name__)
+app.redis = redis.StrictRedis(
+    host='localhost',
+    port=6379, db=0)
+cache = Cache(app, config={'CACHE_TYPE': 'simple'})
+app.url_map.converters['regex'] = RegexConverter
+app.config.from_object('mediachicken.conf')
+app.jinja_env.add_extension('pyjade.ext.jinja.PyJadeExtension')
+
+
+from mediachicken import functions, processors, filters, views
