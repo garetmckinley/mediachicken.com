aa2c5f529110d12c8235d80958000215fdb17d98
Garet McKinley <garetmckinley@me.com>
2014-10-23 17:17:08 -0400

    Added global headers and footers via yml files

--- mediachicken/processors.py
+++ mediachicken/processors.py
@@ -6,12 +6,99 @@ from mediachicken import app
 @app.context_processor
 def utility_processor():
 
-    def import_yml(name):
-        filename = '%s/%s.yml' % (app.config["BASE_DIR"], name)
+    def css(url):
+        return '<link href="%s" rel="stylesheet">\n' % url
+
+    def js(url):
+        return '<script type="text/javascript" src="%s"></script>\n' % url
+
+    def meta(params):
+        tag = '<meta'
+        try:
+            tag += ' name="%s"' % params['name']
+        except:
+            pass
+        try:
+            tag += ' content="%s"' % params['content']
+        except:
+            pass
+        tag += '>\n'
+        print(tag)
+        return tag
+
+    def import_yml(name, dir=None):
+        if dir:
+            filename = '%s/%s/%s.yml' % (app.config["BASE_DIR"], dir, name)
+        else:
+            filename = '%s/%s.yml' % (app.config["BASE_DIR"], name)
 
         if os.path.isfile(filename):
             data = yaml.load(open(filename, 'r'))
             return data
         return dict(err="Failed to load %s" % filename)
 
-    return dict(import_yml=import_yml)
+    def template_header():
+        src =import_yml('header', 'templates')
+        header = ''
+
+
+        def script(content):
+            return '<script type="text/javascript">%s</script>\n' % content
+
+
+        tags = {
+            "css": css,
+            "js": js,
+            "meta": meta,
+            "script": script,
+        }
+
+
+        for tag in src.items():
+            tag, params = tag
+            if type(params) == type(list()):
+                for i in range(0, len(params)):
+                    try:
+                        header += tags[tag](params[i])
+                    except:
+                        print("No parser to process tag of", tag, "type.")
+            else:
+                print(tag, "isn't a list")
+
+        print(header)
+        return header
+
+    def template_footer():
+        src =import_yml('footer', 'templates')
+        footer = ''
+
+
+        def script(content):
+            return '<script type="text/javascript">%s</script>\n' % content
+
+
+        tags = {
+            "css": css,
+            "js": js,
+            "meta": meta,
+            "script": script,
+        }
+
+
+        for tag in src.items():
+            tag, params = tag
+            if type(params) == type(list()):
+                for i in range(0, len(params)):
+                    try:
+                        footer += tags[tag](params[i])
+                    except:
+                        print("No parser to process tag of", tag, "type.")
+            else:
+                print(tag, "isn't a list")
+
+        print(footer)
+        return footer
+
+
+    return dict(import_yml=import_yml, template_header=template_header, template_footer=template_footer)
+
